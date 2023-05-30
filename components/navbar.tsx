c17bbf2a2ed9dc816c1b5af0b925bbbc6511aca5
ThatColdToast <thatcoldtoast@gmail.com>
2023-05-30 18:08:57 -0500

    navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,14 +2,30 @@ import Link from "next/link";
 
 import type { Database } from '@/types/supabase'
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useEffect, useState } from "react";
+
+type User = Database["public"]["Tables"]["profiles"]["Row"];
 
 const Navbar = () => {
   const supabase = createClientComponentClient<Database>()
 
+  const [ user, setUser ] = useState<User>()
+
+  useEffect(() => {
+    const getData = async () => {
+      const userId = (await supabase.auth.getUser()).data.user?.id
+      const profile = (await supabase.from("profiles").select().eq('id', userId)).data?.at(0)
+      setUser(profile);
+    };
+
+    getData();
+  }, []);
+
   return (
     <div className="navbar bg-gray-700 text-white">
-      <div className="flex-1">
+      <div className="flex flex-row justify-start items-center">
         <h1 className="btn btn-ghost normal-case text-xl">Shift Tracker</h1>
+        <p>{user?.username}</p>
       </div>
       <div className="flex-none border-b-2">
         <ul className="flex flex-row">
